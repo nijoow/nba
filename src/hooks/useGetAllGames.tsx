535a2c996fe5af7d1eea46cad18db8f12e1ff721
Woojin Lee <nijoow1127@gmail.com>
2023-02-28 00:03:08 +0900

    🎨 useGetAllGames hook 수정

--- src/hooks/useGetAllGames.tsx
+++ src/hooks/useGetAllGames.tsx
@@ -4,17 +4,28 @@ import React, { useEffect, useState } from "react";
 
 const useGetAllGames = (date: string, page?: number) => {
   const [gameList, setGameList] = useState<AllGamesResponse>();
+  const [loading, setLoading] = useState(false);
   const per_page = 20;
 
   useEffect(() => {
-    axios
-      .get(
-        `https://www.balldontlie.io/api/v1/games?page=${page}&per_page=${per_page}&dates[]=${date}`
-      )
-      .then((res) => setGameList(res.data));
+    getAllGames(date, page);
   }, [page, date]);
 
-  return { gameList };
+  const getAllGames = async (date: string, page?: number) => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `https://www.balldontlie.io/api/v1/games?page=${page}&per_page=${per_page}&dates[]=${date}`
+      );
+      setGameList(response.data);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { gameList, loading };
 };
 
 export default useGetAllGames;
