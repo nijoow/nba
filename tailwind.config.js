37f51f11a45b698055fd4045a3436f51326154a6
Woojin Lee <nijoow1127@gmail.com>
2023-03-01 02:10:01 +0900

    📦️ react-draggable 패키지 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        image: "url('/background.jpg')",
+      },
+    },
   },
   plugins: [],
 };
